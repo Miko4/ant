d8ca563e3536f2d42fa9396411477557cd135138
Miko4 <you@example.com>
2016-04-21 15:42:29 +0200

    try a travis-installation

---
+++ .travis-ci.sh
@@ -0,0 +1,3 @@
+bash .travis-install.sh
+bash build.sh
+./ant
---
+++ .travis-install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install libkpathsea6 libkpathsea-dev
+
+wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sh -s /usr/local/bin
+
+yes Y|opam init
+eval `opam config env`
+opam switch -y  install 3.10.2
+opam install -y omake
+opam install -y  ocamlfind
+opam insatll -y camlimages
